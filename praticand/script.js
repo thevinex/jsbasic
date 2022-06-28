49f10b87ca3dc966cd2799c40f146751ed5d29c5
thevinex <vinirf@live.com>
2022-06-28 14:48:56 -0300

    praticando

---
+++ praticand/script.js
@@ -0,0 +1,35 @@
+//Arrays
+
+// const animals = [
+//     'lion',
+//     'monkey',
+//     'cat',
+//     {
+//         name: 'cat',
+//         age:3
+//     }
+// ]
+// console.log(animals[0])
+// console.log(animals.length)
+// console.log(animals[2].name)
+
+//1
+// let weight
+//2
+// console.log(typeof weight)
+//3
+// let name = 'Vinicius'
+// let age = 25
+// let stars = 12.2
+// let isSubscribed = true
+//4
+// let student ={
+//     name : 'Vinicius',
+//     age : 25,
+//     weight : 56.5,
+//     isSubscribed : true,
+// };
+// console.log(`${student.name} de idade ${student.age} pesa ${student.weight} Kg`)
+//5
+//let students = []
+//
