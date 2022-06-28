32394b41005d64f5550a02fe5cf6e3143820e5bd
thevinex <vinirf@live.com>
2022-06-28 11:31:53 -0300

    dados

---
+++ dados.js
@@ -0,0 +1,29 @@
+// String
+console.log("---Strings---")
+console.log("Ola, mundo!")
+console.log(`Vinicius ${1 + 1}`)
+//Numbers
+console.log("---Numbers---")
+console.log(10.5)
+console.log(Infinity)
+console.log("---Boolean---")
+console.log(true)
+console.log(false)
+console.log("---Undefined and null---")
+console.log(undefined)
+console.log(null)
+console.log("---Object---")
+console.log({
+    name:"Vinicius",
+    idade: 25,
+    andar: function(){
+        console.log('andar')
+    }
+})
+console.log("---Array---")
+console.log([
+    "Leite",
+    "Ovos",
+    2,
+    3
+])
