f21b30fb9b6b0b9fba24c697b9a3469dd0887854
Vinícius Rozendo Ferraz <47616239+thevinex@users.noreply.github.com>
2022-07-04 17:10:01 -0300

    especializar react css fonte componente propriedades estado imutabilidade key prop hooks header useEffect API async

---
+++ reactJS/reactapp/src/pages/home/home.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import {Card} from '../../components/Card/index';
+
+export function Home() {
+
+  const [studentName, setStudentName] =  useState('');
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name: '', avatar: ''});
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleDateString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+    setStudents(prevState => [...prevState,newStudent]);
+  }
+
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch('https://api.github.com/users/thevinex');
+      const data = await response.json();
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+    });
+  }
+  
+  fetchData();
+
+  }, []);
+
+  return(
+    <div className='container'>
+
+      <header>
+        <h1>Lista de presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+      </header>
+
+      <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)}/>
+      <button type="button" onClick={handleAddStudent}>Adicionar</button>
+      {
+        students.map(student => 
+        <Card 
+          key={student.time}
+          name={student.name} 
+          time={student.time}
+        />)
+      }
+      
+    </div>
+  )
+}
