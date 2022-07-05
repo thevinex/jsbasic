28d02609e7cc02d85c7bd528739c99872cb28988
thevinex <vinirf@live.com>
2022-07-05 10:48:40 -0300

    Fundamentos typeScript

--- reactJS/reactapp/src/pages/home/home.tsx
+++ reactJS/reactapp/src/pages/home/home.tsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-import {Card} from '../../components/Card/index';
+import { Card, CardProps } from '../../components/Card/index';
+
+type ProfileResponse = {
+  name: string;
+  avatar_url: string;
+}
+
+type User = {
+  name: string;
+  avatar: string;
+}
 
 export function Home() {
 
   const [studentName, setStudentName] =  useState('');
-  const [students, setStudents] = useState([]);
-  const [user, setUser] = useState({name: '', avatar: ''});
+  const [students, setStudents] = useState<CardProps[]>([]);
+  const [user, setUser] = useState<User>({} as User);
 
   function handleAddStudent(){
     const newStudent = {
@@ -23,7 +33,9 @@ export function Home() {
   useEffect(() => {
     async function fetchData(){
       const response = await fetch('https://api.github.com/users/thevinex');
-      const data = await response.json();
+      const data = await response.json() as ProfileResponse;
+      
+
       setUser({
         name: data.name,
         avatar: data.avatar_url,
